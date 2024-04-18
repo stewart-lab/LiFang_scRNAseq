91fc112ac73c4810d5ad592ada623d436043f246
Moore <bmjohns43@gmail.com>
2024-04-18 10:51:19 -0500

    wip

---
+++ sra_download.sh
@@ -0,0 +1,16 @@
+# download sra accession
+# make sure to configure  
+# vdb-config -i
+# add proxy address for ceph: ceph-dv.morgridge.net
+# day 1
+~/programs/sratoolkit.3.0.5-centos_linux64/bin/fasterq-dump SRR17712089
+~/programs/sratoolkit.3.0.5-centos_linux64/bin/fasterq-dump SRR17712090
+# day 2
+~/programs/sratoolkit.3.0.5-centos_linux64/bin/fasterq-dump SRR17712087
+~/programs/sratoolkit.3.0.5-centos_linux64/bin/fasterq-dump SRR17712088
+# day 3
+~/programs/sratoolkit.3.0.5-centos_linux64/bin/fasterq-dump SRR17712085
+~/programs/sratoolkit.3.0.5-centos_linux64/bin/fasterq-dump SRR17712086
+# day 5
+~/programs/sratoolkit.3.0.5-centos_linux64/bin/fasterq-dump SRR17712083
+~/programs/sratoolkit.3.0.5-centos_linux64/bin/fasterq-dump SRR17712084
---
+++ src/convert_seurat2anndata.R
@@ -0,0 +1,63 @@
+
+library(reticulate)
+library(purrr)
+library(jsonlite)
+library(rmarkdown)
+library(Seurat)
+use_condaenv(condaenv = '/w5home/bmoore/miniconda3/envs/scRNAseq_best/', required = TRUE)
+## to install SeuratDisk
+# first install hdf5r:
+# conda install -c conda-forge r-hdf5r
+# Next install SeuratDisk:
+# conda install -c conda-forge r-seuratdisk
+# or
+# if (!requireNamespace("remotes", quietly = TRUE)) {
+#   install.packages("remotes")
+# }
+# remotes::install_github("mojaveazure/seurat-disk")
+# note for me conda install -c conda-forge r-seuratdisk did not work, but the github install did
+
+library(SeuratDisk)
+# variables
+WD <- "/w5home/bmoore/scRNAseq/LiFangChu/fluidigm_gup_expr_results/output_20240417_134352/"
+SEURAT_OBJ <- "clustered_seurat_obj.rds"
+METADATA_GAMM <- "NA" #"/w5home/bmoore/Gamm_scRNAseq/data/gamm_metadata/gammS2_manual_annot_metadata_c0.5.txt"
+OUTPUT_name <- "clustered_seurat_obj.h5Seurat"
+
+setwd(WD)
+
+
+# read in Seurat object
+seurat <- readRDS(file = SEURAT_OBJ)
+
+if(METADATA_GAMM=="NA"){
+  print("No metadata file provided")
+} else {
+    print("Metadata file provided")
+    print(METADATA_GAMM)
+    print("Reading in metadata")
+    # add in metadata from previous analysis
+    metadata.gamm <- read.csv(METADATA_GAMM, row.names = 1,
+                            header = TRUE, sep = "\t")
+    # check metadata with query data
+    print(colnames(seurat@assays$RNA@data)[1:10])
+    print(rownames(metadata.gamm)[1:10])
+    print(colnames(seurat@meta.data))
+
+    # add metadata to query data
+    seurat <- AddMetaData(seurat, metadata.gamm)
+    print(colnames(seurat@meta.data))
+}
+
+# remove "_1" from metadata
+#rownames(metadata.gamm) <- sub("_1", "", rownames(metadata.gamm))
+#rownames(metadata.gamm)[1:10]
+
+# first save seurat as h5 seurat file
+SaveH5Seurat(seurat, filename = OUTPUT_name)
+# then convert to h5ad
+Convert(OUTPUT_name, dest = "h5ad")
+
+# can now be read in by scanpy:
+# import scanpy
+# adata = scanpy.read_h5ad("pbmc3k.h5ad")
